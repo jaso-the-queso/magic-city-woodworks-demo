6c900805d58a406af40ffdb89c6a0b2e11dd6fa1
Paul <pldobbs@mac.com>
2018-03-29 09:28:39 -0500

    home page fix

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,13 +4,13 @@ import { Link } from 'react-router-dom';
 import Car from './Carousel.jsx';
 
 export default class Home extends Component {
-    
+
     render() {
         return (
             <Fragment>
                 <Car />
                 <div className='container-fluid '>
-                    <h1>asdf</h1>
+                    <h1>Hey Idiot N00b Jason</h1>
                     <h1>asdf</h1>
                     <h1>asdf</h1>
                     <h1>asdf</h1>
@@ -19,7 +19,8 @@ export default class Home extends Component {
                     <h1>asdf</h1>
 
                     <h1>asdf</h1>
-                    <h1>asdf</h1><h1>asdf</h1>
+                    <h1>asdf</h1>
+                    <h1>asdf</h1>
                     <h1>asdf</h1>
                     <h1>asdf</h1>
                     <h1>asdf</h1>
