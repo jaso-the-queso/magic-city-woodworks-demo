5e1ea9394acd5d41b1113c54fb32f51d2681c21e
jaso-the-queso <jasonsimonsdrummer@gmail.com>
2018-03-30 11:30:18 -0500

    changes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,49 +1,28 @@
 import React, { Component, Fragment } from 'react';
 import { Link } from 'react-router-dom';
+import { Button } from 'mdbreact';
 
 import Car from './Carousel.jsx';
 
 export default class Home extends Component {
-
+    
     render() {
         return (
-            <Fragment>
+            <Fragment id='wrap'>
                 <Car />
-                <div className='container-fluid '>
-                    <h1>Hey Idiot N00b Jason</h1>
-                    <h1>asdf</h1>
-                    <h1>asdf</h1>
-                    <h1>asdf</h1>
-                    <h1>asdf</h1>
-                    <h1>asdf</h1>
-                    <h1>asdf</h1>
-
-                    <h1>asdf</h1>
-                    <h1>asdf</h1>
-                    <h1>asdf</h1>
-                    <h1>asdf</h1>
-                    <h1>asdf</h1>
-                    <h1>asdf</h1>
-                    <h1>asdf</h1>
-                    <h1>asdf</h1>
-                    <h1>asdf</h1>
-                    <h1>asdf</h1>
-                    <h1>asdf</h1>
-                    <h1>asdf</h1>
-                    <h1>asdf</h1>
-                    <h1>asdf</h1>
-                    <h1>asdf</h1>
-                    <h1>asdf</h1>
-                    <h1>asdf</h1>
-                    <h1>asdf</h1>
-                    <h1>asdf</h1>
-                    <h1>asdf</h1>
-                    <h1>asdf</h1>
-                    <h1>asdf</h1>
-                    <h1>asdf</h1>
-                    <h1>asdf</h1>
-                    <h1>asdf</h1>
-
+                <div className='container my-4 justify-content-center' id="main">
+                    <div className='row main'>
+                        <div className='col-md-3'>
+                            <p className='invisible'>.</p>
+                        </div>
+                        <div className='col-md-6 border-top border-bottom text-center'>
+                            <h4 className='font-weight-bold py-4'>WE TEACH YOUNG MEN THE ART OF WORK</h4>
+                            <Button outline color="elegant mb-4">LEARN MORE</Button>
+                        </div>
+                        <div className='col-md-3'>
+                            <p className='invisible'>.</p>
+                        </div>
+                    </div>
                 </div>
             </Fragment>
         );
